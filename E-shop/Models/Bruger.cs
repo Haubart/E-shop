8f641054dcb3352bc33e760d23b767d43189ddec
Struklr <rasmus.dalsgaard.andersen@gmail.com>
2022-01-14 13:38:21 +0100

    required

--- E-shop/Models/Bruger.cs
+++ E-shop/Models/Bruger.cs
@@ -11,19 +11,31 @@ namespace E_shop.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
     
     public partial class Bruger
     {
         public int BrugerID { get; set; }
+        [Required(ErrorMessage = "Dette felt er nødvendigt.")]
         public string ForNavn { get; set; }
+        [Required(ErrorMessage = "Dette felt er nødvendigt.")]
         public string EfterNavn { get; set; }
+        [Required(ErrorMessage = "Dette felt er nødvendigt.")]
         public string Adgangskode { get; set; }
+        [Required(ErrorMessage = "Dette felt er nødvendigt.")]
         public string Mail { get; set; }
         public Nullable<bool> status { get; set; }
+        [Required(ErrorMessage = "Dette felt er nødvendigt.")]
         public string Adresse { get; set; }
+        [Required(ErrorMessage = "Dette felt er nødvendigt.")]
         public string Postnr { get; set; }
+        [Required(ErrorMessage = "Dette felt er nødvendigt.")]
         public string By { get; set; }
+        [Required(ErrorMessage = "Dette felt er nødvendigt.")]
         public string Telefon { get; set; }
+        [Required(ErrorMessage = "Dette felt er nødvendigt.")]
         public string Land { get; set; }
+
     }
 }
