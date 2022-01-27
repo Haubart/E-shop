1d5cab40e4b73b6eadc9350b17316378469d918f
Struklr <rasmus.dalsgaard.andersen@gmail.com>
2022-01-27 21:14:25 +0100

    Updates lidt her og der

--- E-shop/Controllers/ShoppingController.cs
+++ E-shop/Controllers/ShoppingController.cs
@@ -119,7 +119,7 @@ namespace E_shop.Controllers
             }
             Session["CartItem"] = null;
             Session["CartCounter"] = null;
-            return RedirectToAction("index");
+            return RedirectToAction("Checkout", "Home");
         }
 
 
